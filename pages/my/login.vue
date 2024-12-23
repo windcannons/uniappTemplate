fda059fc9ca614afe699a3ff6f5502e1dc08c9f6
南极吃火锅 <2717741632@qq.com>
2024-12-23 15:59:16 +0800

    uniapp项目模板

--- pages/my/login.vue
+++ pages/my/login.vue
@@ -40,44 +40,6 @@
                 </text>
             </div>
         </div>
-
-        <div class="popUp" v-if="ispopUp">
-            <div class="mainYhj">
-<div class="yhj">
-    <div class="li">
-        <div class="top">
-            <div class="le">
-                <div class="num">
-                    <text>
-                        ￥
-                    </text>
-                    10
-                </div>
-                <div class="info">
-                    无金额门槛
-                </div>
-            </div>
-            <div class="ri">
-                <div class="name">
-                    <div class="tag">
-                        满减券
-                    </div>
-                    无门槛10员券
-                </div>
-                <div class="time">
-                    2024.11.21-2025.02.30
-                </div>
-            </div>
-        </div>
-        <div class="fot">
-            全场商品通用
-        </div>
-    </div>
-</div>
-                <image @tap="chosePopup" class="close" src="/static/close2.png"></image>
-            </div>
-        </div>
-
     </div>
 </template>
 
@@ -91,19 +53,21 @@ import {
     onLoad,
 } from "@dcloudio/uni-app";
 import {
-    backPage
+    showShareMenu
 } from "../../utils/method";
 
 onLoad(() => {
+    //分享页面
+    showShareMenu()
     uni.login({
         provider: 'weixin',
         success: loginRes => {
-            console.log(loginRes)
             loginInfo.codeLogin = loginRes.code
         }
     })
 })
 
+//登录信息
 let loginInfo = reactive({
     codeLogin: '',
     codePhone: ''
@@ -117,7 +81,6 @@ const agree = (type) => {
 }
 
 //	登录
-let ispopUp = ref(false)
 let isChoose = ref(false)
 const onGetPhoneNumber = (e) => {
     if (!e.detail.encryptedData) {
@@ -125,15 +88,26 @@ const onGetPhoneNumber = (e) => {
     }
     loginInfo.codePhone = e.detail.code
 
-        // 在此调用登录并存入token
-        //uni.setStorageSync('token',res.data.token)
-
+    //调用登录接口传参
+    //weChatPhone({...loginInfo}).then(res => {
+        uni.setStorageSync('token',res.data.token)
+        //newUser
+        //是否新用户  0新用户  1老用户
+        if (Number(res.data.newUser)) {
+            //返回触发登录的页面
+            uni.redirectTo({
+                url: uni.getStorageSync('cachedParams'),
+                fail: function(res) {
+                    uni.switchTab({
+                        url: uni.getStorageSync('cachedParams'),
+                    });
+                }
+            });
+        } else {
+        }
+    //})
 }
 
-const chosePopup = ()=>{
-    ispopUp.value = false
-    backPage()
-}
 </script>
 
 <style scoped lang="less">
@@ -234,112 +208,112 @@ const chosePopup = ()=>{
         display: flex;
         align-items: center;
         justify-content: center;
-.mainYhj{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    .yhj{
-        width: 654rpx;
-        height: 296rpx;
-        background: #FFFFFF;
-        border-radius: 16rpx 16rpx 16rpx 16rpx;
-        padding: 50rpx 35rpx;
+        .mainYhj{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            .yhj{
+                width: 654rpx;
+                height: 296rpx;
+                background: #FFFFFF;
+                border-radius: 16rpx 16rpx 16rpx 16rpx;
+                padding: 50rpx 35rpx;
 
-        .li {
-            width: 100%;
-            height: 100%;
-            background: #EAEAEA;
-            border-radius: 18rpx 18rpx 18rpx 18rpx;
-            border: 1px solid #333333;
-            margin-bottom: 20rpx;
-
-            .top {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                height: 142rpx;
-                border-bottom: 1px dashed #333;
-
-                .le {
-                    width: 228rpx;
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-                    justify-content: center;
-                    border-right: 1px dashed #333;
+                .li {
+                    width: 100%;
                     height: 100%;
+                    background: #EAEAEA;
+                    border-radius: 18rpx 18rpx 18rpx 18rpx;
+                    border: 1px solid #333333;
+                    margin-bottom: 20rpx;
 
-                    .num {
-                        font-weight: bold;
-                        font-size: 72rpx;
-                        color: #333333;
-                        height: 80rpx;
-
-                        text {
-                            font-weight: bold;
-                            font-size: 28rpx;
-                            color: #333333;
-                        }
-                    }
-
-                    .info {
-                        font-size: 24rpx;
-                        color: #333333;
-                    }
-                }
-
-                .ri {
-                    height: 100%;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    padding-left: 18rpx;
-                    flex: 1;
-
-                    .name {
-                        font-size: 24rpx;
-                        color: #333333;
-                        margin-bottom: 28rpx;
+                    .top {
                         display: flex;
                         align-items: center;
+                        justify-content: space-between;
+                        height: 142rpx;
+                        border-bottom: 1px dashed #333;
 
-                        .tag {
-                            width: 83rpx;
-                            height: 32rpx;
-                            background: #333333;
-                            border-radius: 5rpx 5rpx 5rpx 5rpx;
-                            text-align: center;
-                            line-height: 32rpx;
-                            margin-right: 10rpx;
-                            font-size: 20rpx;
-                            color: #FFFFFF;
+                        .le {
+                            width: 228rpx;
+                            display: flex;
+                            flex-direction: column;
+                            align-items: center;
+                            justify-content: center;
+                            border-right: 1px dashed #333;
+                            height: 100%;
+
+                            .num {
+                                font-weight: bold;
+                                font-size: 72rpx;
+                                color: #333333;
+                                height: 80rpx;
+
+                                text {
+                                    font-weight: bold;
+                                    font-size: 28rpx;
+                                    color: #333333;
+                                }
+                            }
+
+                            .info {
+                                font-size: 24rpx;
+                                color: #333333;
+                            }
+                        }
+
+                        .ri {
+                            height: 100%;
+                            display: flex;
+                            flex-direction: column;
+                            justify-content: center;
+                            padding-left: 18rpx;
+                            flex: 1;
+
+                            .name {
+                                font-size: 24rpx;
+                                color: #333333;
+                                margin-bottom: 28rpx;
+                                display: flex;
+                                align-items: center;
+
+                                .tag {
+                                    width: 83rpx;
+                                    height: 32rpx;
+                                    background: #333333;
+                                    border-radius: 5rpx 5rpx 5rpx 5rpx;
+                                    text-align: center;
+                                    line-height: 32rpx;
+                                    margin-right: 10rpx;
+                                    font-size: 20rpx;
+                                    color: #FFFFFF;
+                                }
+                            }
+
+                            .time {
+                                font-size: 20rpx;
+                                color: #333333;
+                            }
                         }
                     }
 
-                    .time {
-                        font-size: 20rpx;
+                    .fot {
+                        padding-left: 34rpx;
+                        height: 62rpx;
+                        line-height: 62rpx;
+                        font-size: 24rpx;
                         color: #333333;
                     }
                 }
             }
 
-            .fot {
-                padding-left: 34rpx;
-                height: 62rpx;
-                line-height: 62rpx;
-                font-size: 24rpx;
-                color: #333333;
+            .close{
+                width: 52rpx;
+                height: 52rpx;
+                margin-top: 28rpx;
             }
         }
-    }
-
-    .close{
-        width: 52rpx;
-        height: 52rpx;
-        margin-top: 28rpx;
-    }
-}
     }
 }
 </style>
